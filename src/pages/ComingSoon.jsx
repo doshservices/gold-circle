df6e7fa7d44fead3ab06c1095a187a2a74125940
prosper edafe <mamusedafe19@gmail.com>
2024-10-14 16:52:35 +0100

    changed font

--- src/pages/ComingSoon.jsx
+++ src/pages/ComingSoon.jsx
@@ -7,7 +7,7 @@ const ComingSoon = () => {
             <div className='px-6'>
                 <img src={appLogo1} alt="app logo" className='h-[84px] mx-auto block' />
                 <h1 className='text-lg mt-12 tracking-[1px]'>WE ARE ALMOST THERE</h1>
-                <h2 className='text-5xl md:text-6xl lg:text-8xl font-maiandra mt-2 mb-12 tracking-[14px]'>COMING <br /> SOON</h2>
+                <h2 className='text-3xl xsm:text-5xl md:text-6xl lg:text-8xl font-maiandra mt-2 mb-12 tracking-[14px]'>COMING <br /> SOON</h2>
                 <p className='text-lg tracking-[1px] max-w-[589px] mx-auto'>Join our newsletter to be the first to know when Gold Circle launches and receive updates on our latest development</p>
                 <form onSubmit={(e) => e.preventDefault()} className='flex gap-3 justify-center max-w-[625px] mx-auto mt-16'>
                     <input type="email" placeholder='Email Address' className='text-sm border border-[#F8F8F8] rounded-[4px] text-[#F8F8F899] bg-transparent py-3 px-4 grow' />
